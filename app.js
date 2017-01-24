76b81ad84f4a6d48c0d1ec36e2ac430295361aca
cbooz <cboozar@gmail.com>
2017-01-24 17:30:17 -0500

    adding app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+{
+  "name": "Automation Pipeline",
+  "description": "A barebones Node.js app",
+  "repository": "https://github.com/cboozar/automation_practice",
+  "logo": "https://auto-pipeline.herokuapp.com/node.png",
+  "keywords": ["node", "static"]
+}
